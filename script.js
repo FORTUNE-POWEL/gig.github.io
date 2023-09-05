19dcfab1ad48f7c926b57c4758c565acfd6cbd30
FORTUNE-POWEL <85201032+FORTUNE-POWEL@users.noreply.github.com>
2023-09-05 16:06:34 +0300

    Add files via upload
    added service and work pages

--- script.js
+++ script.js
@@ -63,3 +63,76 @@ function prevSlide() {
 setInterval(nextSlide, 5000); // Auto slide every 5 seconds
 
 */
+
+
+//  the service pricing designs
+console.clear();
+
+const cardsContainer = document.querySelector(".cards");
+const cardsContainerInner = document.querySelector(".cards__inner");
+const cards = Array.from(document.querySelectorAll(".card"));
+const overlay = document.querySelector(".overlay");
+
+const applyOverlayMask = (e) => {
+    const overlayEl = e.currentTarget;
+    const x = e.pageX - cardsContainer.offsetLeft;
+    const y = e.pageY - cardsContainer.offsetTop;
+
+    overlayEl.style = `--opacity: 1; --x: ${x}px; --y:${y}px;`;
+};
+
+const createOverlayCta = (overlayCard, ctaEl) => {
+    const overlayCta = document.createElement("div");
+    overlayCta.classList.add("cta");
+    overlayCta.textContent = ctaEl.textContent;
+    overlayCta.setAttribute("aria-hidden", true);
+    overlayCard.append(overlayCta);
+};
+
+const observer = new ResizeObserver((entries) => {
+    entries.forEach((entry) => {
+        const cardIndex = cards.indexOf(entry.target);
+        let width = entry.borderBoxSize[0].inlineSize;
+        let height = entry.borderBoxSize[0].blockSize;
+
+        if (cardIndex >= 0) {
+            overlay.children[cardIndex].style.width = `${width}px`;
+            overlay.children[cardIndex].style.height = `${height}px`;
+        }
+    });
+});
+
+const initOverlayCard = (cardEl) => {
+    const overlayCard = document.createElement("div");
+    overlayCard.classList.add("card");
+    createOverlayCta(overlayCard, cardEl.lastElementChild);
+    overlay.append(overlayCard);
+    observer.observe(cardEl);
+};
+
+cards.forEach(initOverlayCard);
+document.body.addEventListener("pointermove", applyOverlayMask, false);
+
+function dollars() {
+    var listItems = document.getElementsByClassName("card__price").innerHTML;
+    listItems = "$...";
+}
+
+listItems.addEventListener("onload", dollars, false);
+
+// the work page content display
+
+
+const bigGuy = querySelector(".portfo");
+
+function display() {
+    var page = document.getElementById("content1");
+    if (page.style.display === "none" || page.style.display === "") {
+        page.style.display = "block";
+    } else {
+        page.style.display = "none";
+    }
+    console.log(display());
+}
+
+bigGuy.addEventListener("click", display, false);
