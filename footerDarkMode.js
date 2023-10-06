6411cc051012d6c0310911ec097aa5ad2b9fcf50
Fortune Powel <powelfortune@gmail.com>
2023-10-06 18:25:32 +0300

    dark mode footer with still a bug in the nav bar image

--- footerDarkMode.js
+++
@@ -1,7 +0,0 @@
-const topFooter = document.getElementById('thirty_one');
-const mainFooter = document.getElementById('mainFooter');
-const bottomFooter = document.getElementById('tryOut');
-
-mainFooter.style.backgroundColor = "#171717";
-topFooter.style.backgroundColor = "#171717";
-bottomFooter.style.backgroundColor = '#171717';
