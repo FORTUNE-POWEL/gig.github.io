c4fd6ee3c05cae64a5c64ba42d49b5282bd805e6
Fortune Powel <powelfortune@gmail.com>
2023-10-06 13:43:29 +0300

    bug in the nav

---
+++ footerDarkMode.js
@@ -0,0 +1,7 @@
+const topFooter = document.getElementById('thirty_one');
+const mainFooter = document.getElementById('mainFooter');
+const bottomFooter = document.getElementById('tryOut');
+
+mainFooter.style.backgroundColor = "#171717";
+topFooter.style.backgroundColor = "#171717";
+bottomFooter.style.backgroundColor = '#171717';
